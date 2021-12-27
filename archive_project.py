959831187def0c57be0e6767bcc26c417f462821
Hrishikesh Ballal <hrishikeshballal@yahoo.com>
2021-12-27 22:10:55 +0000

    Added underscore python style for variables

--- archive_project.py
+++ archive_project.py
@@ -50,115 +50,118 @@ if __name__ == "__main__":
       sys.exit(1)
 
     try:
-      assert c.keys() ==set(['serviceurl', 'projectid', 'apitoken'])
+      assert c.keys() ==set(['service_url', 'project_ids', 'api_token'])
     except AssertionError as ae: 
       logger.error("Error in config file parameters")
       sys.exit(1)
-
-    my_api_helper = GeodesignHub.GeodesignHubClient(url = c['serviceurl'], project_id= c['projectid'], token=c['apitoken'])
-    # make project folder
-    project_directory = Path("output", c['projectid'])
-    project_directory.mkdir(parents=True, exist_ok=True)
-
-    zip_file_name = c['projectid']
-    zip_file_directory = Path("output", zip_file_name)
-
-    # Get all Systems
-    all_projects_response = my_api_helper.get_project_id()
-    if all_projects_response.status_code == 200:
-      all_project_details = all_projects_response.json()
-      
-      print("Project data downloaded")
-      df = pd.read_json(json.dumps(all_project_details), typ='series')
-      logger.info("Writing Project data file to disk..")
-      df.to_csv(Path.joinpath(project_directory, "project.csv"))
-      logger.info("Project data file written")
-    else: 
-      logger.error("Error in getting systems data from Geodesignhub: %s " % all_projects_response.text)
     
-    # Get all Systems
-    all_systems_response = my_api_helper.get_all_systems()
-    if all_systems_response.status_code == 200:
-      all_systems = all_systems_response.json()
-      all_system_details = []
-      for system in all_systems:
-        system_detail_response = my_api_helper.get_single_system(system['id'])
-        try: 
-          assert system_detail_response.status_code == 200
-        except AssertionError as ae: 
-          logger.error("Error in getting System Details %s" % system_detail_response.error)
-        else:
-          all_system_details.append(system_detail_response.json())
-  
-      print("Systems data downloaded")
-      df = pd.read_json(json.dumps(all_system_details))
-      logger.info("Writing Systems file to disk..")
-      df.to_csv(Path.joinpath(project_directory, "systems.csv"))
-      logger.info("Systems file written")
-    else: 
-      logger.error("Error in getting systems data from Geodesignhub: %s " % all_systems_response.text)
-      
-    # Get all Diagrams 
-    all_diagrams_response = my_api_helper.get_all_diagrams()
-    if all_diagrams_response.status_code==200:
-      all_diagrams = all_diagrams_response.json()
-      print("Diagrams data downloaded")
-      df = pd.read_json(json.dumps(all_diagrams))
+    project_ids = c['project_ids']
+    for project_id in project_ids:
 
-      logger.info("Writing diagrams to disk..")
-      df.to_csv(Path.joinpath(project_directory,  "diagrams.csv"))
-      logger.info("Diagrams file written")
-    else: 
-      logger.error("Error in getting systems data from Geodesignhub: %s " % all_systems_response.text)
+      my_api_helper = GeodesignHub.GeodesignHubClient(url = c['service_url'], project_id= c['project_id'], token=c['api_token'])
+      # make project folder
+      project_directory = Path("output", c['project_id'])
+      project_directory.mkdir(parents=True, exist_ok=True)
+
+      zip_file_name = c['project_id']
+      zip_file_directory = Path("output", zip_file_name)
+
+      # Get all Systems
+      all_projects_response = my_api_helper.get_project_id()
+      if all_projects_response.status_code == 200:
+        all_project_details = all_projects_response.json()
+        
+        print("Project data downloaded")
+        df = pd.read_json(json.dumps(all_project_details), typ='series')
+        logger.info("Writing Project data file to disk..")
+        df.to_csv(Path.joinpath(project_directory, "project.csv"))
+        logger.info("Project data file written")
+      else: 
+        logger.error("Error in getting systems data from Geodesignhub: %s " % all_projects_response.text)
       
-    # Get all Design Teams
-    all_design_team_details = []
-    all_design_team_response = my_api_helper.get_all_design_teams()
-    if all_design_team_response.status_code == 200:
-      all_design_teams = all_design_team_response.json()
+      # Get all Systems
+      all_systems_response = my_api_helper.get_all_systems()
+      if all_systems_response.status_code == 200:
+        all_systems = all_systems_response.json()
+        all_system_details = []
+        for system in all_systems:
+          system_detail_response = my_api_helper.get_single_system(system['id'])
+          try: 
+            assert system_detail_response.status_code == 200
+          except AssertionError as ae: 
+            logger.error("Error in getting System Details %s" % system_detail_response.error)
+          else:
+            all_system_details.append(system_detail_response.json())
+    
+        print("Systems data downloaded")
+        df = pd.read_json(json.dumps(all_system_details))
+        logger.info("Writing Systems file to disk..")
+        df.to_csv(Path.joinpath(project_directory, "systems.csv"))
+        logger.info("Systems file written")
+      else: 
+        logger.error("Error in getting systems data from Geodesignhub: %s " % all_systems_response.text)
+        
+      # Get all Diagrams 
+      all_diagrams_response = my_api_helper.get_all_diagrams()
+      if all_diagrams_response.status_code==200:
+        all_diagrams = all_diagrams_response.json()
+        print("Diagrams data downloaded")
+        df = pd.read_json(json.dumps(all_diagrams))
+
+        logger.info("Writing diagrams to disk..")
+        df.to_csv(Path.joinpath(project_directory,  "diagrams.csv"))
+        logger.info("Diagrams file written")
+      else: 
+        logger.error("Error in getting systems data from Geodesignhub: %s " % all_systems_response.text)
+        
+      # Get all Design Teams
       all_design_team_details = []
-      for design_team in all_design_teams:
-        design_team_detail_response = my_api_helper.get_all_details_for_design_team(design_team['id'])
-        try: 
-          assert design_team_detail_response.status_code == 200
-        except AssertionError as ae: 
-          logger.error("Error in getting Design Team Details %s" % design_team_detail_response.text)
-        else:
-          all_design_team_details.append(design_team_detail_response.json())
-  
-      print("Design Team data downloaded")
-      df = pd.read_json(json.dumps(all_design_team_details))
-      logger.info("Writing  Design Team file to disk..")
-      df.to_csv(Path.joinpath(project_directory, "design_teams.csv"))
-      logger.info(" Design Team file written")
-    else: 
-      logger.error("Error in getting  Design Team data from Geodesignhub: %s " % all_design_team_response.text)
+      all_design_team_response = my_api_helper.get_all_design_teams()
+      if all_design_team_response.status_code == 200:
+        all_design_teams = all_design_team_response.json()
+        all_design_team_details = []
+        for design_team in all_design_teams:
+          design_team_detail_response = my_api_helper.get_all_details_for_design_team(design_team['id'])
+          try: 
+            assert design_team_detail_response.status_code == 200
+          except AssertionError as ae: 
+            logger.error("Error in getting Design Team Details %s" % design_team_detail_response.text)
+          else:
+            all_design_team_details.append(design_team_detail_response.json())
     
-    # Get all Synthesis and diagrams
-    all_design_syntheses_and_diagrams = []
-    for current_team_synthesis in all_design_team_details:
-      all_current_team_details = current_team_synthesis['synthesis']
+        print("Design Team data downloaded")
+        df = pd.read_json(json.dumps(all_design_team_details))
+        logger.info("Writing  Design Team file to disk..")
+        df.to_csv(Path.joinpath(project_directory, "design_teams.csv"))
+        logger.info(" Design Team file written")
+      else: 
+        logger.error("Error in getting  Design Team data from Geodesignhub: %s " % all_design_team_response.text)
       
-      for current_team_details in all_current_team_details:        
-        current_team_id = int(current_team_details['cteamid'])
-        synthesis_id = current_team_details['id']
-        synthesis_name = current_team_details['description']
-        synthesis_digrams_response = my_api_helper.get_single_synthesis_diagrams(teamid = current_team_id, synthesisid = synthesis_id)
-        try: 
-          assert synthesis_digrams_response.status_code == 200
-        except AssertionError as ae: 
-          logger.error("Error in getting Diagram Details %s" % synthesis_digrams_response.text)
-        else:
-          synthesis_and_diagrams = synthesis_digrams_response.json()
-          synthesis_and_diagrams['description'] = synthesis_name
-          all_design_syntheses_and_diagrams.append(synthesis_and_diagrams)
+      # Get all Synthesis and diagrams
+      all_design_syntheses_and_diagrams = []
+      for current_team_synthesis in all_design_team_details:
+        all_current_team_details = current_team_synthesis['synthesis']
+        
+        for current_team_details in all_current_team_details:        
+          current_team_id = int(current_team_details['cteamid'])
+          synthesis_id = current_team_details['id']
+          synthesis_name = current_team_details['description']
+          synthesis_digrams_response = my_api_helper.get_single_synthesis_diagrams(teamid = current_team_id, synthesisid = synthesis_id)
+          try: 
+            assert synthesis_digrams_response.status_code == 200
+          except AssertionError as ae: 
+            logger.error("Error in getting Diagram Details %s" % synthesis_digrams_response.text)
+          else:
+            synthesis_and_diagrams = synthesis_digrams_response.json()
+            synthesis_and_diagrams['description'] = synthesis_name
+            all_design_syntheses_and_diagrams.append(synthesis_and_diagrams)
 
-    df = pd.read_json(json.dumps(all_design_syntheses_and_diagrams))
-    logger.info("Writing  Design Team data file to disk..")
-    
-    df.to_csv(Path.joinpath(project_directory, "design_syntheses.csv"))
-    logger.info("Design Team file written")
-    print("Design Team and diagrams written")
+      df = pd.read_json(json.dumps(all_design_syntheses_and_diagrams))
+      logger.info("Writing  Design Team data file to disk..")
+      
+      df.to_csv(Path.joinpath(project_directory, "design_syntheses.csv"))
+      logger.info("Design Team file written")
+      print("Design Team and diagrams written")
 
-    shutil.make_archive(Path('output', zip_file_name), 'zip', zip_file_directory)
-    shutil.rmtree(project_directory)
+      shutil.make_archive(Path('output', zip_file_name), 'zip', zip_file_directory)
+      shutil.rmtree(project_directory)
